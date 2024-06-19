ce7d66dd6bede0032c4bb0216a130bf6609bb96b
SantiLecter <130516055+SantiLecter@users.noreply.github.com>
2024-06-19 15:47:55 -0500

    Delete Adivina_el_codigo-main/java directory

--- Adivina_el_codigo-main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module example.adivina_el_codigo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens example.adivina_el_codigo to javafx.fxml;
-    exports example.adivina_el_codigo;
-}
